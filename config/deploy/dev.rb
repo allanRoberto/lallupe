e3d7fea2bb293376637c248501863850efa4d72a
Allan Roberto <Allan@MacBook-Pro-de-Allan.local>
2016-07-28 11:05:56 -0300

    Atualizando configuração do servidor SSH

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -5,11 +5,11 @@ set :deploy_to, "/home/artezzo/sites/#{application}-#{stage}"
 set :branch, "dev" 
 
 # The hostnames to deploy to.
-role :web, "artezzo.com.br" 
+role :web, "web961.uni5.net" 
 
 # Specify one of the web servers to use for database backups or updates.
 # This server should also be running Wordpress.
-role :db, "artezzo.com.br", :primary => true 
+role :db, "web961.uni5.net", :primary => true 
 
 # The path to wp-cli
 set :wp, "wp"  #(Should be the full path to the wp command on your server)
