3624eedec82a560cc0a0659783114675fcddec77
Allan Roberto <allan.rsti@gmail.com>
2016-08-04 17:06:11 -0300

    Update produtos

--- wordpress/wp-content/themes/storefront-child/functions.php
+++ wordpress/wp-content/themes/storefront-child/functions.php
@@ -16,7 +16,7 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
 
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
-
+  
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sku', 10);
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
