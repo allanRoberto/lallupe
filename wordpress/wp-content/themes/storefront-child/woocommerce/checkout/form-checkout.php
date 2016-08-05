9c7c5702344301956d500c901fb5de467b0d4820
Allan Roberto <allan.rsti@gmail.com>
2016-08-05 18:54:17 -0300

    Arrunando o checkout

--- wordpress/wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
+++ wordpress/wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
@@ -15,12 +15,9 @@
  * @package 	WooCommerce/Templates
  * @version     2.3.0
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-
 if(!is_user_logged_in()) { ?>	
 	<div class="login-access">	
 		<div class="row">
@@ -92,15 +89,12 @@ if(!is_user_logged_in()) { ?>
 
 <?php
 //wc_print_notices();
-
 do_action( 'woocommerce_before_checkout_form', $checkout );
-
 // If checkout registration is disabled and not logged in, the user cannot checkout
 if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_user_logged_in() ) {
 	//echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) );
 	return;
 }
-
 ?>
 
 
