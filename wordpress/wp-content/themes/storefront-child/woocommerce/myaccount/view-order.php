4cf5e70a38114388f2be617365a91e6896b900c6
Allan Roberto <allan.rsti@gmail.com>
2016-08-11 20:08:28 -0400

    Status do pedido

--- wordpress/wp-content/themes/storefront-child/woocommerce/myaccount/view-order.php
+++ wordpress/wp-content/themes/storefront-child/woocommerce/myaccount/view-order.php
@@ -17,14 +17,216 @@
  * @package WooCommerce/Templates
  * @version 2.6.0
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-wc_get_order_statuses();
+	$template_url = get_template_directory_uri().'-child/app/';
+	$status = wc_get_order_statuses();
+	$status_name = wc_get_order_status_name( $order->get_status() );
+	switch ($status_name) {
+		case 'Pagamento Pendente':?>
+			<ul class="navigation-status">
+				<li class="order-status-payment-pending active">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+					<span class="title-order-status">Pagamento pendente</span>
+				</li>
+				<li class="order-status-payment-aproved">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+					<span class="title-order-status">Pagamento aprovado</span>
+				</li>
+				<li class="order-status-processing">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+					<span class="title-order-status">Processando pedido</span>
+				</li>
+				<li class="order-status-shipping">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+					<span class="title-order-status">Pedido em transporte</span>
+				</li>
+				<li class="order-status-complete">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+					<span class="title-order-status">Pedido concluído</span>
+				</li>
+			</ul>
+			<?php break;
+		case 'Pagamento em Análise':?>
+			<ul class="navigation-status">
+				<li class="order-status-payment-pending active">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+					<span class="title-order-status">Pagamento pendente</span>
+				</li>
+				<li class="order-status-payment-aproved">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+					<span class="title-order-status">Pagamento aprovado</span>
+				</li>
+				<li class="order-status-processing">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+					<span class="title-order-status">Processando pedido</span>
+				</li>
+				<li class="order-status-shipping">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+					<span class="title-order-status">Pedido em transporte</span>
+				</li>
+				<li class="order-status-complete">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+					<span class="title-order-status">Pedido concluído</span>
+				</li>
+			</ul>
+			<?php break;
+		case 'Pagamento aprovado':?>
+			<ul class="navigation-status">
+				<li class="order-status-payment-pending active">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+					<span class="title-order-status">Pagamento pendente</span>
+				</li>
+				<li class="order-status-payment-aproved">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+					<span class="title-order-status">Pagamento aprovado</span>
+				</li>
+				<li class="order-status-processing">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+					<span class="title-order-status">Processando pedido</span>
+				</li>
+				<li class="order-status-shipping">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+					<span class="title-order-status">Pedido em transporte</span>
+				</li>
+				<li class="order-status-complete">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+					<span class="title-order-status">Pedido concluído</span>
+				</li>
+			</ul>
+			<?php break;	
+		case 'Processando pedido':?>
+			<ul class="navigation-status">
+				<li class="order-status-payment-pending active">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+					<span class="title-order-status">Pagamento pendente</span>
+				</li>
+				<li class="order-status-payment-aproved">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+					<span class="title-order-status">Pagamento aprovado</span>
+				</li>
+				<li class="order-status-processing">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+					<span class="title-order-status">Processando pedido</span>
+				</li>
+				<li class="order-status-shipping">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+					<span class="title-order-status">Pedido em transporte</span>
+				</li>
+				<li class="order-status-complete">
+					<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+					<span class="title-order-status">Pedido concluído</span>
+				</li>
+			</ul>
+			<?php break;
+		
+			case 'Pedido em transporte' : ?>
+				<ul class="navigation-status">
+					<li class="order-status-payment-pending active">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+						<span class="title-order-status">Pagamento pendente</span>
+					</li>
+					<li class="order-status-payment-aproved">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+						<span class="title-order-status">Pagamento aprovado</span>
+					</li>
+					<li class="order-status-processing">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+						<span class="title-order-status">Processando pedido</span>
+					</li>
+					<li class="order-status-shipping">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+						<span class="title-order-status">Pedido em transporte</span>
+					</li>
+					<li class="order-status-complete">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+						<span class="title-order-status">Pedido concluído</span>
+					</li>
+				</ul>
+			<?php break;
+		
+			case 'Pedido entregue': ?>
+				<ul class="navigation-status">
+					<li class="order-status-payment-pending active">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+						<span class="title-order-status">Pagamento pendente</span>
+					</li>
+					<li class="order-status-payment-aproved">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+						<span class="title-order-status">Pagamento aprovado</span>
+					</li>
+					<li class="order-status-processing">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+						<span class="title-order-status">Processando pedido</span>
+					</li>
+					<li class="order-status-shipping">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+						<span class="title-order-status">Pedido em transporte</span>
+					</li>
+					<li class="order-status-complete">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+						<span class="title-order-status">Pedido concluído</span>
+					</li>
+				</ul>
+			<?php break;
+			case 'Pedido em análise' : ?>
+				<ul class="navigation-status">
+					<li class="order-status- active">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-.png"/>
+						<span class="title-order-status">Pedido cancelado</span>
+					</li>
+				</ul>
+			<?php break;
+		
+			case 'Pedido Cancelado' :?>
+				<ul class="navigation-status">
+					<li class="order-status-canceled active">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-canceled.png"/>
+						<span class="title-order-status">Pedido cancelado</span>
+					</li>
+				</ul>
+			<?php break;
+			
+			case 'Pedido reembolsado' :?>
+				<ul class="navigation-status">
+					<li class="order-status-payment-pending active">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-pending.png"/>
+						<span class="title-order-status">Pagamento pendente</span>
+					</li>
+					<li class="order-status-payment-aproved">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-payment-aproved.png"/>
+						<span class="title-order-status">Pagamento aprovado</span>
+					</li>
+					<li class="order-status-processing">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-processing.png"/>
+						<span class="title-order-status">Processando pedido</span>
+					</li>
+					<li class="order-status-shipping">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-shipping.png"/>
+						<span class="title-order-status">Pedido em transporte</span>
+					</li>
+					<li class="order-status-complete">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-complete.png"/>
+						<span class="title-order-status">Pedido concluído</span>
+					</li>
+					<li class="order-status-return">
+						<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-return.png"/>
+						<span class="title-order-status">Pedido reembolsado</span>
+					</li>
+				</ul>
+			<?php break;
+			
+			case 'Pedido Falhado' :?>
+				<ul>
+				 <li class="order-status-failed active">
+				 	<img class="img-responsive" src="<?php echo $template_url ?>/images/order-status-failed.png"/>
+				 	<span class="title-order-status">Pedido falhado</span>
+				 </li>
+				</ul>
+			<?php break;
+	}
 ?>
-<p class="order-status"><span class="status-order">Status do pedido  : <?php echo wc_get_order_status_name( $order->get_status() ); ?></span></p>
 <p class="info-order">
 	<span>Número do pedido : <mark><?php echo $order->get_order_number(); ?></mark></span>
 	<span>Data do pedido : <mark><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></mark></span>
